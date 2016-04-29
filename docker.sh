f8fbf6b16cc6a90dd35ec69a4688e229afee4276
alexey-ernest <alexey.ernest@gmail.com>
2016-04-29 22:19:20 +0600

    Quick deploy script fix

--- docker.sh
+++ docker.sh
@@ -2,4 +2,4 @@ docker stop auction
 docker rm auction
 docker rmi auction
 docker build -t auction .
-docker run -d --name auction -p 8080:8080 -e POSTGRES_CONNECTION=postgres://auction:auction@192.168.99.100:5432/auction -e SESSION_SECRET=abc -e DEBUG=auction-game:* auction
+docker run -d --name auction -p 8080:8080 -e POSTGRES_CONNECTION=postgres://auction:auction@192.168.99.100:5432/auction -e TOKEN_SECRET=secret -e DEBUG=auction-game:* auction
