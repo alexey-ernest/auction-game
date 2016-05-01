c6588d5a4f8f08eaa8fab165abc02f7ee7669ac0
alexey-ernest <alexey.ernest@gmail.com>
2016-05-02 05:45:22 +0600

    Inventory endpoint refactoring

--- routes/api/inventory/index.js
+++ routes/api/inventory/index.js
@@ -7,34 +7,15 @@ var router = express.Router();
 
 var auth = require('../../../lib/middleware/auth');
 var db = require('../../../lib/db');
-var Inventory = db.Inventory;
+var getInventory = require('./get-inventory')(db);
 
-/**
- * Helper method for mapping db inventory items.
- *
- * @method     mapInventoryItem
- * @param      {Object}  i       Inventory object.
- * @return     {Object}  Mapped Object.
- */
-function mapInventoryItem(i) {
-  return {
-    id: i.id,
-    item: i.item,
-    quantity: i.quantity
-  };
-}
 
 /**
  * Get player inventory items.
  */
 router.get('/',
   auth.private(),
-  function (req, res, next) {
-    Inventory.getPlayerItems(req.player.id, function (err, items) {
-      if (err) return next(err);
-      res.json(items.map(mapInventoryItem));
-    });
-  });
-
+  getInventory
+  );
 
 module.exports = router;
