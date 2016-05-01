3c64143d93091a15f4bdd9ac21a60879b3b12a87
alexey-ernest <alexey.ernest@gmail.com>
2016-05-02 05:46:50 +0600

    Remove unnecessary mapping

--- routes/api/inventory/get-inventory.js
+++ routes/api/inventory/get-inventory.js
@@ -2,28 +2,13 @@
  * @fileOverview GET /api/inventory endpoint.
  */
 
-/**
- * Helper method for mapping db inventory items.
- *
- * @method     mapInventoryItem
- * @param      {Object}  i       Inventory object.
- * @return     {Object}  Mapped Object.
- */
-function mapInventoryItem(i) {
-  return {
-    id: i.id,
-    item: i.item,
-    quantity: i.quantity
-  };
-}
-
 module.exports = function (db) {
   var Inventory = db.Inventory;
 
 	return function (req, res, next) {
     Inventory.getPlayerItems(req.player.id, function (err, items) {
       if (err) return next(err);
-      res.json(items.map(mapInventoryItem));
+      res.json(items);
     });
   };
 };
